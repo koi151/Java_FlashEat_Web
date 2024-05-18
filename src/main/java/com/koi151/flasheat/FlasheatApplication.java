a1403d8825cf456d00683f59dba531cf976a6b71
Khôi Nguyễn <originalkhoi151@gmail.com>
2024-05-18 12:08:23 +0700

    configured Keylogger, added saving log file feature

--- src/main/java/com/koi151/flasheat/FlasheatApplication.java
+++ src/main/java/com/koi151/flasheat/FlasheatApplication.java
@@ -2,10 +2,10 @@ package com.koi151.flasheat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 public class FlasheatApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(FlasheatApplication.class, args);
 	}
